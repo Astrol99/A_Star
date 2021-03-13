ecdb49c27291be54007deafa569f3eccdaf93cd9
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-03-13 00:49:13 -0600

    Added basic node array

--- astar.js
+++ astar.js
@@ -1,8 +1,22 @@
+/// <reference path="./p5.global-mode.d.ts" />
+
+const size = 5;
+
+let nodes = [];
+
 function setup() {
     createCanvas(800, 600);
     background(0, 0, 0);
+
+    for (let x = size; x < width; x += size*2) {
+        for (let y = size; y < height; y += size*2) {
+            let node = new Node(x, y, size, "gray", "open");
+            node.render();
+            nodes.push(node);
+        }
+    }
 }
 
 function draw() {
-    ellipse(mouseX, mouseY, 50);
+
 }
